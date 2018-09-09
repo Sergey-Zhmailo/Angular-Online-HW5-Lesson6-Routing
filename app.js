18a63c8bebdc0b40bce91789a3c89d634ac34059
Sergey Zhmailo <sergey.seoz@gmail.com>
2018-09-09 22:44:51 +0300

    lesson9 | deploy Heroku v2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist/routing-app')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/routing-app'));
+  res.sendFile(path.join(__dirname, 'dist/routing-app/index.html'));
 });
 
 const port = process.env.PORT || '3000';
